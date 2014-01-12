f3e23e123f27cf6d47cf5bff8904277eff585fa1
julien lengrand-lambert <julien@lengrand.fr>
2014-01-12 18:30:26 +0100

    Adds support for several projects

--- server/checkStatus.py
+++ server/checkStatus.py
@@ -8,16 +8,24 @@ from jenkinsapi import build
 
 port = "/dev/ttyACM0"
 baudrate = 9600
-#url = 'http://localhost:8080'
 url = 'http://192.168.1.20:8080'
-project = 'test_1'
+
 
 FAIL = "f"
 BUILD = "b"
 SUCCESS = "s"
 frequency = 2
 
-lastState = ''
+project_1 = 'test_1'
+project_2 = "test_2"
+
+# each project has an identifier, to be sent using the serial link as well
+id_1 = "1"
+id_2 = "2"
+
+lastState_1= ''
+lastState_2= ''
+
 j = jenkins.Jenkins(url)
 
 if len(sys.argv) == 3:
@@ -30,11 +38,13 @@ else:
 # the arduino need some time to warm up
 time.sleep(4)
 
-def check():
+def checkAll():
+        check(project_1, id_1, lastState_1)
+
+def check(project, id, lastState):
         try:
                 job = j.get_job(project)
 
-                print job
                 # builds which are queued or running are not listed in the jenkins
                 # dictionary, so at this time the job.__build__ related calls fails by
                 # reference error
@@ -45,27 +55,24 @@ def check():
                 currentBuild = build.Build(current['url'], current['number'], job)
 
                 if currentBuild.is_running():
-                        light(BUILD)
-                        return
-
-                if currentBuild.is_good():
-                        light(SUCCESS)
-                        return
-
-                light(FAIL)
+                        light(BUILD, id, lastState)
+                elif currentBuild.is_good():
+                        light(SUCCESS, id, lastState)
+                else:
+                        light(FAIL, id, lastState)
         except:
-                print "Error"
+                print "Server Side Error"
                 print sys.exc_info()[1]
-                light(FAIL)
+                #light(FAIL)
+                #TODO: Another led later for server side errors
 
-def light(state):
-        global lastState
-
+def light(state, id, lastState):
+        msg = id
         if state != lastState:
-                print lastState, state
-                output.write(state)
+                msg += state
+                output.write(msg)
                 lastState = state
 
 while True:
-        check()
+        checkAll()
         time.sleep(frequency)
